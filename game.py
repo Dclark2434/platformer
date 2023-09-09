235fa3659e9f0e4e8508e2f0fc5ec193377a1f1a
Dustin Clark <dclark2434@gmail.com>
2023-09-08 22:50:47 -0500

    added tiles

--- game.py
+++ game.py
@@ -1,16 +1,17 @@
 import pygame
 
 import sys
-
 from scripts.utils import load_image
+from scripts.utils import load_images
 from scripts.entities import PhysicsEntity
-
+from scripts.tilemap import Tilemap
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Platformer")
         # Create the screen
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240)) # the display is the surface that the player sees. It is smaller than the screen to create a pixelated effect
 
         # Create the clock object to slow down the code
         self.clock = pygame.time.Clock()
@@ -18,15 +19,22 @@ class Game:
         self.movement = [False, False]
 
         self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15)) # create a player entity
+
+        self.tilemap = Tilemap(self, tile_size=16) # create a tilemap object (see scripts/tilemap.py
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248)) # refill the screen with a sky blue color every frame to keep moving images from leaving a trail
+            self.display.fill((14, 219, 248)) # refill the screen with a sky blue color every frame to keep moving images from leaving a trail
+            self.tilemap.render(self.display) # render the tilemap
             self.player.update((self.movement[1] - self.movement[0], 0)) # update the player entity
-            self.player.render(self.screen) # render the player entity
+            self.player.render(self.display) # render the player entity
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -41,7 +49,7 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_d:
                         self.movement[1] = False
-
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)) # scale the display surface to the size of the screen and blit it to the screen
             pygame.display.update()
             self.clock.tick(60) # 60 frames per second
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 
 BASE_IMG_PATH = 'data/images/'
@@ -6,3 +7,9 @@ def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
     return img
+
+def load_images(path):
+    images  = []
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)): # loop through all the files in the folder and sort them alphabetically
+        images.append(load_image(path + '/' + img_name)) # load the image and append it to the list
+    return images
